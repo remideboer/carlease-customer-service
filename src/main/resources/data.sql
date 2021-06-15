4153a00c26d3806c2f37f06f6bd1b63b11784dc4
Remi de Boer <r.d.d.de.boer@hva.nl>
2021-06-15 20:14:00 +0200

    added sql insert script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into klant (voornaam, tussenvoegsel, achternaam, straat_naam, postcode, huisnummer, toevoeging, woonplaats,
+                   emailadres, telefoonnummer)
+values ('test01', 'tussen01', 'achter01', 'weg01', '1111AA', 1, null, 'stad01', 'test01@test.nl', '0611111111');
+insert into klant (voornaam, tussenvoegsel, achternaam, straat_naam, postcode, huisnummer, toevoeging, woonplaats,
+                   emailadres, telefoonnummer)
+values ('test02', null, 'achter02', 'weg02', '2222BB', 2, 'bis', 'stad02', 'test02@test.nl', '0622222222');
+
